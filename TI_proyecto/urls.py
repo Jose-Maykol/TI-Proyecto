e0cdc84e98010d61c8beb24a1b57d0585e900672
Joel Gutiérrez <64146055+joel123-gothub@users.noreply.github.com>
2022-07-09 13:41:23 -0500

    Update urls.py

--- TI_proyecto/urls.py
+++ TI_proyecto/urls.py
@@ -6,7 +6,9 @@ from django.conf import settings
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
-    path('users/', include('users.urls'))
+    path('users/', include('users.urls')),
+    path('servicios/', include('servicios.urls')),
+     path('admin/', admin.site.urls),
 ]
 
 urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
