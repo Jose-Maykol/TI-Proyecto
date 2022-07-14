100451d23199d539a7e7c345c5acc835a8181f60
José Maykol <jpaniura@unsa.edu.pe>
2022-07-13 21:34:07 -0500

    Update homepage

--- TI_proyecto/settings.py
+++ TI_proyecto/settings.py
@@ -57,7 +57,8 @@ TEMPLATES = [
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
             os.path.join(BASE_DIR, "templates"),
-            os.path.join(BASE_DIR, "users/templates")
+            os.path.join(BASE_DIR, "users/templates"),
+            os.path.join(BASE_DIR, "homepage/templates")
         ],
         'APP_DIRS': True,
         'OPTIONS': {
--- homepage/views.py
+++ homepage/views.py
@@ -4,5 +4,5 @@ from django.views.generic import TemplateView
 
 
 class HomeView(TemplateView):
-  template_name = 'index.html'
+  template_name = 'homepage.html'
 
